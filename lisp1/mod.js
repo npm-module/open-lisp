3a62d86b09ed53482aa7566363b5e4896dfdfd92
JavaCommons Technologies <javacommons@gmail.com>
2026-03-07 15:30:17 +0900

    # Commit at 2026.0307.1530.16 [https://github.com/npm-module/open-lisp] [DIFF] diff --git a/lisp1/mod.js b/lisp1/mod.js     -  return "npm:open-lisp: version 2026.307.151434";     +  return "npm:open-lisp: version 2026.307.151604"; [/DIFF]

--- lisp1/mod.js
+++ lisp1/mod.js
@@ -99,7 +99,7 @@ export class system {
 }
 
 export function version() {
-  return "npm:open-lisp: version 2026.307.151434";
+  return "npm:open-lisp: version 2026.307.151604";
 }
 
 export function versionNumber() {
