be2cc5ce1112c433c7f19c767790ca7ca9f94199
JavaCommons Technologies <javacommons@gmail.com>
2026-03-08 13:05:45 +0900

    # Commit at 2026.0308.1305.43 [https://github.com/npm-module/open-lisp] [DIFF] diff --git a/lisp1/init.sh b/lisp1/init.sh     -#cd "$(dirname "$0")"     +cd "$(dirname "$0")"     -#deno add npm:prettier@3.8.1 [/DIFF]

--- lisp1/init.sh
+++ lisp1/init.sh
@@ -1,10 +1,9 @@
 #! /usr/bin/env bash
 set -uvx
 set -e
-#cd "$(dirname "$0")"
+cd "$(dirname "$0")"
 cwd=`pwd`
 deno add jsr:@std/assert
 deno add jsr:@std/fs
 deno add jsr:@deno/dnt
-#deno add npm:prettier@3.8.1
 deno add npm:js-beautify@1.15.4
